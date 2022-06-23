346bb2a6c0d1f11bec4f45194fa72b65d4f2383c
omobayode6 <omobayode93@gmail.com>
2022-06-23 17:52:44 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = { 
+    fullName: "Omobayode Festus",
+    bio: "A web developer with a very strong visual perception of design, ❤ simplicity, and white spaces.",
+    imgSrc: 'https://www.pngitem.com/pimgs/m/146-1468281_profile-icon-png-transparent-profile-picture-icon-png.png',
+    profession: "Full Stack Engineer",
+    isShow: true,
+   }
+
+   toggleShow = ()=>{
+    //changing the isShow state to true
+    this.setState({ isShow: !this.state.isShow});
+   }
+
+
+  render() { 
+    return (
+      <div className="App">
+        {this.state.isShow && 
+        <div>
+          <img src= {this.state.imgSrc} alt={this.state.fullName} />
+          <h2>{this.state.fullName}</h2>
+          <h4>{this.state.profession}</h4>
+          <p> {this.state.bio} </p>
+        </div>
+        }
+        
+        <button onClick = {this.toggleShow}> {this.state.isShow ? "Click to hide" : "Click to show" }</button>
+     </div>
+    );
+  }
 }
+ 
 
 export default App;
