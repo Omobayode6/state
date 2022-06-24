2ffcf77ed6493d1f816e4abb55f2ae254f917342
omobayode6 <omobayode93@gmail.com>
2022-06-24 20:10:43 +0100

    commiting updates

--- src/App.js
+++ src/App.js
@@ -3,18 +3,39 @@ import './App.css';
 
 
 class App extends Component {
-  state = { 
+  constructor(){
+    super()
+  this.state = { 
     fullName: "Omobayode Festus",
     bio: "A web developer with a very strong visual perception of design, ❤ simplicity, and white spaces.",
     imgSrc: 'https://www.pngitem.com/pimgs/m/146-1468281_profile-icon-png-transparent-profile-picture-icon-png.png',
     profession: "Full Stack Engineer",
     isShow: true,
+    count: 0,
+   }
+   this.toggleShow = this.toggleShow.bind(this)
+  }
+
+  
+
+   toggleShow(){
+    //changing the isShow state to true
+    this.setState({ 
+      isShow: !this.state.isShow,
+      count: this.state.count
+    });
    }
 
-   toggleShow = ()=>{
-    //changing the isShow state to true
-    this.setState({ isShow: !this.state.isShow});
-   }
+
+   componentDidMount() {
+     setInterval(() => {
+      this.setState(() => {
+        return {
+          count: this.state.count + 1,
+        };
+      });
+    }, 1000);
+  }
 
 
   render() { 
@@ -26,10 +47,12 @@ class App extends Component {
           <h2>{this.state.fullName}</h2>
           <h4>{this.state.profession}</h4>
           <p> {this.state.bio} </p>
+          <p>This component was updated {this.state.count} seconds ago</p> 
         </div>
         }
-        
         <button onClick = {this.toggleShow}> {this.state.isShow ? "Click to hide" : "Click to show" }</button>
+         
+
      </div>
     );
   }
